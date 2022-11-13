c09757103e860a2292262804afcf49cbb8f754c3
b21827588 <b21827588@cs.hacettepe.edu.tr>
2022-11-14 01:01:02 +0300

    scrolls for message textarea

--- assignment2/src/ViewMessagePage.java
+++ assignment2/src/ViewMessagePage.java
@@ -14,13 +14,13 @@ public class ViewMessagePage extends Page implements ActionListener {
 
     private Message message;
 
-    public ViewMessagePage(Controller controller,Message message) {
+    public ViewMessagePage(Controller controller, Message message) {
         this.controller = controller;
         setJFrame(new JFrame("Message"));
         getJFrame().setResizable(false);
         getJFrame().setLayout(null);
         getJFrame().setBounds(300, 90, 600, 600);
-        this.container =  getJFrame().getContentPane();
+        this.container = getJFrame().getContentPane();
 
         this.messageArea = new JTextArea();
         this.messageArea.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -29,7 +29,7 @@ public class ViewMessagePage extends Page implements ActionListener {
         this.messageArea.setEditable(false);
 
         this.messageArea.setText(message.getContent());
-        this.messageArea.setBorder(BorderFactory.createLineBorder(Color.black,1));
+        this.messageArea.setBorder(BorderFactory.createLineBorder(Color.black, 1));
 
         this.container.add(this.messageArea);
 
@@ -40,11 +40,25 @@ public class ViewMessagePage extends Page implements ActionListener {
         this.returnButton.setLocation(230, 450);
         this.returnButton.addActionListener(this);
 
+        this.messageArea.setAutoscrolls(true);
+
+
         this.container.add(this.returnButton);
+
+
+        JScrollPane scroll = new JScrollPane(messageArea,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroll.setSize(400, 350);
+        scroll.setLocation(100, 50);
+
+        getJFrame().add(scroll);
+
+
         getJFrame().setVisible(true);
 
     }
-    public void setMessage(Message message){
+
+    public void setMessage(Message message) {
         this.messageArea.setText(message.getContent());
     }
 
